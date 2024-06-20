84563d46a4483573ba7469b21ce96b0281466fdf
oracle312 <oracle312@naver.com>
2024-06-20 15:04:10 +0900

    delete treeview duplication

--- testDB/testDB/Main.cs
+++ testDB/testDB/Main.cs
@@ -32,7 +32,37 @@ namespace testDB
         {
             tree_member.Nodes.Clear();
 
-            try
+            MySqlConnection conn = new MySqlConnection("Server = localhost;Database=testdb;Uid=root;Pwd=0000;");
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM account", conn);
+            da.Fill(dt);
+
+            /*foreach (DataRow dr in dt.Rows)
+            {
+                TreeNode nod = tree_member.Nodes.Find(dr["user_team"].ToString(), false);
+                if (nod == null)
+                {
+                    nod = new TreeNode(dr["user_team"].ToString());
+                    tree_member.Nodes.Add(nod);
+                }
+                nod.Nodes.Add(dr["user_name"].ToString());
+            }*/
+
+            
+
+            int i = 0;
+            foreach (IGrouping<string, string> user_team in dt.Rows.Cast<DataRow>().GroupBy(dr => dr["user_team"].ToString(), dr => dr["user_name"].ToString()))
+            {
+                TreeNode nod = new TreeNode(user_team.Key);
+
+                foreach (string user_name in user_team)
+                    nod.Nodes.Add(user_name);
+
+                tree_member.Nodes.Add(nod);
+                i++; 
+            }
+
+            /*try
             {
                 MySqlConnection conn = new MySqlConnection("Server = localhost;Database=testdb;Uid=root;Pwd=0000;");
 
@@ -41,12 +71,13 @@ namespace testDB
                 MySqlCommand cmd = new MySqlCommand(selectQuery, conn);
                 MySqlDataReader dr = cmd.ExecuteReader();
 
-                DataTable dt = new DataTable();
-                MySqlDataAdapter da = new MySqlDataAdapter(selectQuery, conn);
-                da.Fill(dt);
+                
 
                 while (dr.Read())
                 {
+
+
+
                     TreeNode tn = new TreeNode(dr["user_team"].ToString());
                     tn.Nodes.Add(dr["user_name"].ToString() + " " + dr["user_rank"].ToString());
                     tree_member.Nodes.Add(tn);
@@ -58,9 +89,12 @@ namespace testDB
             catch (Exception except)
             {
                 MessageBox.Show(except.Message);
-            }
+            }*/
             sql_connect.Enabled = false;
             
         }
+
+      
+        
     }
 }
