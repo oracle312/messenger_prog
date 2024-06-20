4c7e0839cbc7a1573986421111ec0e506e3cac5f
oracle312 <oracle312@naver.com>
2024-06-20 13:17:20 +0900

    add treeview

---
+++ testDB/testDB/Main.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace testDB
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label_userinfo = new MetroFramework.Controls.MetroLabel();
+            this.label_lastlogin = new MetroFramework.Controls.MetroLabel();
+            this.tree_member = new System.Windows.Forms.TreeView();
+            this.sql_connect = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label_userinfo
+            // 
+            this.label_userinfo.AutoSize = true;
+            this.label_userinfo.Location = new System.Drawing.Point(23, 60);
+            this.label_userinfo.Name = "label_userinfo";
+            this.label_userinfo.Size = new System.Drawing.Size(81, 19);
+            this.label_userinfo.TabIndex = 2;
+            this.label_userinfo.Text = "metroLabel1";
+            // 
+            // label_lastlogin
+            // 
+            this.label_lastlogin.AutoSize = true;
+            this.label_lastlogin.Location = new System.Drawing.Point(23, 401);
+            this.label_lastlogin.Name = "label_lastlogin";
+            this.label_lastlogin.Size = new System.Drawing.Size(81, 19);
+            this.label_lastlogin.TabIndex = 3;
+            this.label_lastlogin.Text = "metroLabel1";
+            // 
+            // tree_member
+            // 
+            this.tree_member.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tree_member.Location = new System.Drawing.Point(23, 82);
+            this.tree_member.Name = "tree_member";
+            this.tree_member.Size = new System.Drawing.Size(160, 260);
+            this.tree_member.TabIndex = 4;
+            // 
+            // sql_connect
+            // 
+            this.sql_connect.Tick += new System.EventHandler(this.sql_connect_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 440);
+            this.Controls.Add(this.tree_member);
+            this.Controls.Add(this.label_lastlogin);
+            this.Controls.Add(this.label_userinfo);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Chat";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public MetroFramework.Controls.MetroLabel label_userinfo;
+        public MetroFramework.Controls.MetroLabel label_lastlogin;
+        private System.Windows.Forms.TreeView tree_member;
+        private System.Windows.Forms.Timer sql_connect;
+    }
+}
---
+++ testDB/testDB/Main.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace testDB
+{
+    public partial class Main : MetroFramework.Forms.MetroForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public Main(Login login)
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            sql_connect.Enabled = true;
+        }
+
+        private void sql_connect_Tick(object sender, EventArgs e)
+        {
+            tree_member.Nodes.Clear();
+
+            try
+            {
+                MySqlConnection conn = new MySqlConnection("Server = localhost;Database=testdb;Uid=root;Pwd=0000;");
+
+                conn.Open();
+                string selectQuery = "SELECT * FROM account order by num asc;";
+                MySqlCommand cmd = new MySqlCommand(selectQuery, conn);
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                MySqlDataAdapter da = new MySqlDataAdapter(selectQuery, conn);
+                da.Fill(dt);
+
+                while (dr.Read())
+                {
+                    TreeNode tn = new TreeNode(dr["user_team"].ToString());
+                    tn.Nodes.Add(dr["user_name"].ToString() + " " + dr["user_rank"].ToString());
+                    tree_member.Nodes.Add(tn);
+
+                    
+                }
+                conn.Close();
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show(except.Message);
+            }
+            sql_connect.Enabled = false;
+            
+        }
+    }
+}
